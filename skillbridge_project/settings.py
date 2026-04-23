bf93ef5e6760964bfe49ab2d9ca938212310158f
ParadoxSolver <sahilsachdev02@gmail.com>
2026-04-24 00:22:08 +0530

    Update settings.py

--- skillbridge_project/settings.py
+++ skillbridge_project/settings.py
@@ -48,9 +48,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+    
+    # Add this line carefully:
+    'allauth.account.middleware.AccountMiddleware', 
 ]
-
 ROOT_URLCONF = 'skillbridge_project.urls'
 
 TEMPLATES = [
